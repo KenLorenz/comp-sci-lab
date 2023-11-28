cc6371f5ec4f383667187cad8bbf737e467acf09
kenlorenz <kenlorenz420@gmail.com>
2023-11-28 20:18:36 +0800

    training phase corrected

--- neural_test.py
+++ neural_test.py
@@ -1,3 +1,5 @@
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,12 +17,12 @@ from keras.callbacks import ReduceLROnPlateau, EarlyStopping
 from keras.utils import to_categorical #.np_utils
 from keras import Input
 
-print('-- phase 1 --')
+print('-- phase 1: Reading dataset --')
 # phase 1
 csv_file= pd.read_csv('dataset/A_Z Handwritten Data.csv').astype('float32')
 dataset = pd.DataFrame(csv_file)
 
-print('-- phase 2 --')
+print('-- phase 2: Preparing dataset --')
 # phase 2
 x = dataset.drop('0', axis = 1)
 y = dataset['0']
@@ -30,38 +32,37 @@ print(f'y: {y.shape}\n\n')
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
 
-x_train = np.reshape(x_train.values, (x_train.shape[0], 28, 28,1))
+#x_train = np.reshape(x_train.values, (x_train.shape[0], 28, 28,1))
 y_train = np.reshape(y_train.values, (y_train.shape[0], 1))
 
-print('-- phase 3 --')
+print('-- phase 3: Preparing model--')
 # phase 3
-
 
 print(f'{x_train.shape}')
 
 # tf.random.set_seed(1234)
 model = Sequential(
     [               
-        tf.keras.Input(shape=(28,28)),
+        tf.keras.Input(shape=(784,)),
         
         #Dense(30, activation='relu', name='L1'), # 28
         Dense(128, activation='relu', name='L2'), # 14
         Dense(64, activation='relu', name='L3'), # 7
-        Dense(1, activation='softmax', name='L4') # 1
+        Dense(26, activation='softmax', name='L4') # 1
         
     ], name = "my_model" 
 )
 
 
-print('-- phase 4 --')    
+print('-- phase 4: Training model --')    
 # phase 4
 
 model.compile(
-    loss=tf.keras.losses.BinaryCrossentropy(),
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), # learning_rate=1
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
 )
 
 model.fit(
     x_train, y_train,
-    epochs=1 # iteration
+    epochs=1
 )
